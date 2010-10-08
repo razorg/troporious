a43a6d275ff29c93783a5dd415eb53b5ad8b5316
Damianos Mylonakis <razor@RAZPC-LIN.localdomain>
2010-10-08 22:49:40 +0300

    delete not needed

--- troporious/apps/smsform/urls.py
+++
@@ -1,6 +0,0 @@
-from tipfy import Rule, import_string
-
-def get_rules(app):
-    rules = [Rule('/testform', endpoint='testform', handler='apps.smsform.handlers.TestFormHandler'),
-    ]
-    return rules
